8df02927c9e0d70d426e6106d2cd01b9c6c6842c
Angus McDonald <angusmcdonald13@gmail.com>
2023-02-09 10:17:03 +1100

    numvalid method

--- Beginner/Control Flow/Control Flow/Program.cs
+++ Beginner/Control Flow/Control Flow/Program.cs
@@ -2,11 +2,20 @@
 
 namespace Control_Flow
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            // testing this for github 
+
         }
+
+        public static void Numvalid()
+        {
+            Console.WriteLine("Enter number between 1 and 10: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num > 0 && num < 11 ? "Valid" : "Invalid");
+        }
+
+
     }
 }
