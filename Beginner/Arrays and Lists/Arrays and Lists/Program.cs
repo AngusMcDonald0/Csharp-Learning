ca7c30c5b40e88e37dc8b0f31a961072c9f83f94
Angus McDonald <angusmcdonald13@gmail.com>
2023-02-12 19:49:22 +1100

    restructure and likes method

--- Beginner/Arrays and Lists/Arrays and Lists/Program.cs
+++ Beginner/Arrays and Lists/Arrays and Lists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
@@ -6,10 +7,28 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            
+            Reverse();
         }
 
-        public static void First()
+        public static void Likes()
+        {
+            var names = new List<string>();
+            while (true)
+            {
+                Console.WriteLine("Who liked you post?");
+                var input = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(input)) break;
+                names.Add(input);
+            }
+            if (names.Count > 2)
+                Console.WriteLine("{0}, {1} and {2} others liked your post", names[0], names[1], names.Count - 2);
+            else if (names.Count == 2)
+                Console.WriteLine("{0} and {1} liked your post", names[0], names[1]);
+            else
+                Console.WriteLine("{0} liked your post", names[0]);
+        }
+
+        public static void Reverse()
         {
 
         }
