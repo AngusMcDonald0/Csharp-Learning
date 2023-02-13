4c65603534a5a18bd2f5df33763d97930fbfef7c
Angus McDonald <angusmcdonald13@gmail.com>
2023-02-13 22:04:30 +1100

    consecutive check method

---
+++ Beginner/String Manipulation/String Manipulation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace String_Manipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Consecutive();
+        }
+
+        public static void Consecutive()
+        {
+            Console.WriteLine("Enter a few - seperated numbers");
+            var input = Console.ReadLine();
+            var numbers = new List<int>();
+            foreach (var num in input.Split("-"))
+            {
+                numbers.Add(Convert.ToInt32(num));
+            }
+            bool consecutive = true;
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                if (numbers[i] > numbers[i+1])
+                    consecutive = false;
+            }
+            Console.WriteLine(consecutive ? "Consecutive" : "Non-Consecutive");
+        }
+    }
+}
