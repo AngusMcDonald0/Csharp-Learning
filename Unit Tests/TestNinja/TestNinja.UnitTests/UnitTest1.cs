03810cadd68b0a730fd1e18374c1b6e1419df322
Angus McDonald <angusmcdonald13@gmail.com>
2023-03-24 22:51:08 +1100

    wrote first unit test

--- Unit Tests/TestNinja/TestNinja.UnitTests/UnitTest1.cs
+++ Unit Tests/TestNinja/TestNinja.UnitTests/UnitTest1.cs
@@ -1,11 +1,30 @@
+
+using TestNinja.Fundamentals;
+
 namespace TestNinja.UnitTests
 {
     [TestClass]
-    public class UnitTest1
+    public class ReservationTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
         {
+            // Arrange
+            var reservation = new Reservation();
+            // Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true});
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        public void CanBeCancelledBy_ReservationMadeByUser_ReturnsTrue()
+        {
+
+        }
+
+        public void CanBeCancelledBy_NotMadeByUserOrAdmin_ReturnsTrue()
+        {
+
         }
     }
 }
