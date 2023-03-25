89605c61142b57b4a60d76555af08eb89a43e11e
Angus McDonald <angusmcdonald13@gmail.com>
2023-03-25 15:00:34 +1100

    error logging tests including exceptions and events raised

--- Unit Tests/TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
+++ Unit Tests/TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -6,10 +6,38 @@ namespace TestNinja.UnitTests
     [TestFixture]
     internal class ErrorLoggerTests
     {
+        private ErrorLogger _Logger;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _Logger = new ErrorLogger();
+        }
+
         [Test]
         public void Log_WhenCalled_SetTheLastErrorProperty() 
         {
+            _Logger.Log("a");
 
+            Assert.That(_Logger.LastError, Is.EqualTo("a"));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowArgumentNullException(string error) 
+        {
+            Assert.That(() => _Logger.Log(error), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Log_ValidError_RaiseErrorLoggedEvent()
+        {
+            var id = Guid.Empty;
+            _Logger.ErrorLogged += (sender, args) => { id = args; };
+            _Logger.Log("a");
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
         }
     }
 }
