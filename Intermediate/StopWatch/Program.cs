8936fe67f0d354a6d663640ccd46d48be61783b8
Angus McDonald <angusmcdonald13@gmail.com>
2023-03-27 10:31:35 +1100

    basic stopwatch functional

---
+++ Intermediate/StopWatch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("stopwatch is ready: ");
+            var input = Console.ReadLine();
+
+            if (input == "start")
+            {
+                var stopWatch = new StopWatch();
+                stopWatch.startTime = DateTime.Now;
+                Console.WriteLine("Hit enter to stop");
+                var enter = Console.ReadLine();
+                if (enter == "")
+                {
+                    stopWatch.stopTime = DateTime.Now;
+                    var count = stopWatch.TimeElapsed();
+                    Console.WriteLine("Time elapsed:" + count);
+                }
+            }
+        }
+    }
+}
---
+++ Intermediate/StopWatch/StopWatch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    internal class StopWatch
+    {
+        public DateTime startTime { get; set; }
+        public DateTime stopTime { get; set; }
+
+        public TimeSpan TimeElapsed() 
+        {
+            var timeElapsed = stopTime - startTime;
+            return timeElapsed;
+        }
+    }
+}
