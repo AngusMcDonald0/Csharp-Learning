60cf1eff94a4194a0b21f5d64e42b30b2b1db5ba
Angus McDonald <angusmcdonald13@gmail.com>
2023-03-26 15:46:55 +1100

    fizzbuzz tests

---
+++ Unit Tests/TestNinja/TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    internal class FizzBuzzTests
+    {
+        [Test]
+        [TestCase (15, "FizzBuzz")]
+        [TestCase (6, "Fizz")]
+        [TestCase (10, "Buzz")]
+        [TestCase (2, "2")]
+        public void GetOutput_IntegerDivisibleByThreeAndFive_ReturnAppropriateString(int input, string expectedResult)
+        {
+            var result = FizzBuzz.GetOutput(input);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
