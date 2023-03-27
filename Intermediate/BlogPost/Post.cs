90d9d4fde4179ae1b77fadcdb69ab2cfce78d2d5
Angus McDonald <angusmcdonald13@gmail.com>
2023-03-27 14:15:13 +1100

    blogpost model and console app complete

--- Intermediate/BlogPost/Post.cs
+++ Intermediate/BlogPost/Post.cs
@@ -8,5 +8,32 @@ namespace BlogPost
 {
     internal class Post
     {
+        private string? _title;
+        private string? _description;
+        private DateTime _createdAt;
+        private int _votes;
+
+        public Post(string title, string description)
+        {
+            _title = title;
+            _description = description;
+            _createdAt = DateTime.Now;
+            _votes = 0;
+        }
+
+        public void Upvote() 
+        {
+            _votes += 1;
+        }
+
+        public void Downvote()
+        {
+            _votes -= 1;
+        }
+
+        public int GetVotes()
+        {
+            return _votes;
+        }
     }
 }
--- Intermediate/BlogPost/Program.cs
+++ Intermediate/BlogPost/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace BlogPost
+{
+    class Program
+    {
+        static void Main(String[] args) 
+        {
+            var allAboutClasses = new Post("lesson on classes", "i like this lesson on classes it seems very useful");
+            allAboutClasses.Upvote();
+            allAboutClasses.Upvote();
+            allAboutClasses.Downvote();
+            Console.WriteLine($"Votes for this post: {allAboutClasses.GetVotes()}");
+        } 
+    }
+}
