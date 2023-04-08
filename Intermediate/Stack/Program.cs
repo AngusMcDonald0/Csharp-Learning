fcb4fff191182788bd5f54eabba49448f3b0aba5
Angus McDonald <angusmcdonald13@gmail.com>
2023-04-08 16:08:20 +1000

    stack features complete

--- Intermediate/Stack/Program.cs
+++ Intermediate/Stack/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
---
+++ Intermediate/Stack/Stack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class Stack
+    {
+        private readonly List<object> _stack = new();
+
+        public void Push (object obj) 
+        {
+            if (obj == null)
+            {
+                throw new InvalidOperationException();
+            }
+            _stack.Add(obj);
+        }
+
+        public object Pop()
+        {
+            if (_stack.Count < 1)
+            {
+                throw new InvalidOperationException();
+            } 
+            var index = _stack.Count - 1;
+            var obj = _stack[index];
+            _stack.RemoveAt(index);
+            return obj;
+        }
+
+        public void Clear()
+        {
+            _stack.Clear();
+        }
+    }
+}
